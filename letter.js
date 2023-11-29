fcc9573afef951d6997386fe04a8bcd8d54b2f9a
sitan516 <sitan516@gmail.com>
2023-11-30 00:01:12 +0900

    gg

--- letter.js
+++ letter.js
@@ -53,6 +53,16 @@ async function decryptText(encryptedText, key) {
 }
 
 
+function enc(){
+    (async () => {
+        // encryptedText = 'TKieZsaOy82AFG2pvqnN6Q=='
+        txt = ''
+        key = 0
+        const encryptedText = await encryptText(txt, key);
+        console.log("암호화된 텍스트:", encryptedText);
+    })();
+    return decryptText
+}
 
 function decryptDay1(key){
     (async () => {
@@ -64,12 +74,43 @@ function decryptDay1(key){
     return decryptText
 }
 
-function enc(key){
+function decryptDay2(key){
     (async () => {
-        // encryptedText = 'TKieZsaOy82AFG2pvqnN6Q=='
-        const encryptedText = await encryptText('txt', 1234123412341234);
-        console.log("암호화된 텍스트:", encryptedText);
+        encryptedText = 'jlbnpt9rtqD5Qs1fU9fdtNl7WQY7uolYUsnvBDvi4BLBlcThD2BbvheM9MELfVsUN/mLwVjPu8mnYdUe/XSxLmQeFjiZ/lvGOLhIkAOF7X+MDWvSFI0x+g2d4hyklPOElpsIz83m+B0y9wwCZpTfDb9aCgrNd6X/twjiWDo9MmuwfgF03LOyyqz3j0lmax92NxlWIHZlMysiaEmLQ5Ch+l/M7CMyqRHxGn3TK353lJi3A5ZRPCw32aHeKrx8p/9eCbLup0ZcUpI1iWr1rkY6i5raXn2btfg7Eez1QzTeikvPCYOOMTVXmrFc3n5KBzYH1BJL0G93rlZuDKGn3ZbgxGPq2pMieIrlhv5ETu0AIj5a7FATL2KXcc05Q4oRQvJw1zQle6m4LfwOJ88TQ3wdxqQVv809DVG8CoFZGuUed9KlBo5e+aBi74lv3wdWs8rVfmCnXmIIY9EW6Yyrks2kM7JH47CN6IMCc1pAx1pXDkmFyk41tRICyXYh3RPtY8Lv9CzFRTbeQUA2OhjafO+Ot73vUQmwBdr8EWaYheaAwaM='
+    
+        const decryptedText = await decryptText(encryptedText, key);
+        console.log("복호화된 텍스트:", decryptedText);
     })();
     return decryptText
 }
 
+function decryptDay3(key){
+    (async () => {
+        console.log(key)
+        encryptedText = 'RciHRUdWyLwwDq+B5tN5auex/u6J+J4jqHcbr5D4GMg16/L59kNC5MTlRat1DqoKVE+mmU0+g7uKQ4hqJgID4oJGe8US71MzQEaoBrqjDZDLCwVUZ30PhoaRlFzLw2Ciggp8tlPEFQhYb6QW66snaY/9glsYTtbGTAHjQct5KGC9ZNfNqpxGpVfHZtM6pdqIpnMdXq0M7Y9+zJwEbaYmzUDlsI1V6yuKvtfxxKC97Svp9rLa6kwp0vWFvvwPqrKltKxeEH7RnLFHlolrHI2+4qNKuYkXe4QnbYhDyLCnpmtdcdyD9fMXQV4JeJyudzHsllsq5AK0oXk2ewETXd/hOfAtGWEc64H6fqRltuSojllBS/LYAz/GHS+zr0ksjs1kKv7+ksBaFGzR/9aaK/yXiA=='  
+    
+        const decryptedText = await decryptText(encryptedText, key);
+        console.log("복호화된 텍스트:", decryptedText);
+    })();
+    return decryptText
+}
+
+function decryptDay4(key){
+    (async () => {
+        encryptedText = 'pHQYntyS6I9cTF9iOERlOmrQyrPTPKh90/BgnMMVgoxoquemWaGDhXZ5HS/9VW5hvzI6B8B4eYgkzOcdBm7x1G4sX3OWuon+QnKemnmvEOspIem2hWMeYiyfwg60cHLtuO1rM5F9YvP88ExaDkPA3izXCVoVGZCB5Cxk5762BfrDV0Xh4Y/JfVJ+ZobIhDmuQ46jq43VrxdCe9xsDkEnxXDvKZCHvl4eH7LJfsUwfTDm3ROuv7U9zt/MYQOOKDZ+H7pSiT61T0oz26Yw9HJGMaNzL/vZk9DIYg9foKbt+QV2iCO5tJ5FKyvDMqpG0QIa0pKOjtcdFur6hyOTQokWE0CLtXO82sQzI8F5IEeuwca3ikN3JJN2MbKnESK3X69D'
+    
+        const decryptedText = await decryptText(encryptedText, key);
+        console.log("복호화된 텍스트:", decryptedText);
+    })();
+    return decryptText
+}
+
+function decryptDay5(key){
+    (async () => {
+        encryptedText = 'xi/EFoE5OLHQqB0+UGQLuCDzuIae6eV4d9aLe36u/j3QvFBklQqjOScaa18u/Hjgl4nzq/PIQyWtevEQEUNPiwjmJzIaAqKkgMaRTYfBV5QatIwiuU3JBws9udUESu5AHSfi5H9sAGwv59XpAtO3jSnPA6UDIXCJ+/PR1tvoW4E2w7/DgfY5uqcV1yu46gfsXKEpjoByXpU1UmfoBAdozx3lG5r32P9na3P6OiJBuEbaCVd5KQRPaysmGfxZFrAyyDyje/gHG8YpOPnP3m/qo/V44/mqtmWRPgyCVR0GkDkPGoF1gvYZK7Hgc3P+YikZawlY13p5/gh5A2rpVidN7Pos4RZ0AlfM9Dmbmfwp80I='  
+    
+        const decryptedText = await decryptText(encryptedText, key);
+        console.log("복호화된 텍스트:", decryptedText);
+    })();
+    return decryptText
+}
--- quiz.js
+++ quiz.js
@@ -9,10 +9,160 @@ function closeModal() {
     document.getElementById('modalOverlay').style.display = 'none';
 }
 
-function quizDay1(){
-    openModal()
-    console.log('12')
-    document.getElementById('quizText').textContent = '우리가 처음 만난 날은?'
+function hash(str) {
+    let hash = 0;
+    for (let i = 0; i < str.length; i++) {
+        const char = str.charCodeAt(i);
+        hash = (hash << 5) - hash + char;
+    }
+    return hash;
 }
 
-document.getElementById('day1').addEventListener('click', quizDay1);
+
+function answerDay1(){
+    key = document.getElementById('answer').value
+    if(hash(key) == '2368947401'){
+        new_key = key.toString() + key.toString()
+        decryptDay1(parseInt(new_key))
+    }else{
+        closeModal()
+    }
+}
+
+function answerDay2(){
+    key = document.getElementById('answer').value
+    if(hash(key) == '2368946442'){
+        new_key = key.toString() + key.toString()
+        decryptDay2(parseInt(new_key))
+    }else{
+        closeModal()
+    }
+}
+
+function answerDay3(){
+    key = document.getElementById('answer').value
+    hashed_key = hash(key).toString()
+    if(hash(hashed_key) == '271051354'){
+        new_key = hashed_key.toString() + hashed_key.toString()
+        decryptDay3(parseInt(new_key.substring(0, 16)))
+    }else{
+        closeModal()
+    }
+}
+
+function answerDay4(){
+    key = document.getElementById('answer').value
+    hashed_key = hash(key).toString()
+    if(hash(hashed_key) == '-2217549493'){
+        new_key = hashed_key.toString() + hashed_key.toString() + hashed_key.toString()
+        decryptDay4(parseInt(new_key.substring(0, 16)))
+    }else{
+        closeModal()
+    }
+}
+
+function answerDay5(){
+    key = document.getElementById('answer').value
+    hashed_key = hash(key).toString()
+    if(hash(hashed_key) == '-2162166201'){
+        new_key = hashed_key.toString() + hashed_key.toString() + hashed_key.toString()
+        decryptDay5(parseInt(new_key.substring(0, 16)))
+    }else{
+        closeModal()
+    }
+}
+
+function quizDay1(){
+    openModal()
+    document.getElementById('quizText').textContent = '우리가 처음 사귄 날은? (YYYYMMDD)'
+    document.getElementById('submitAnswer').addEventListener('click', answerDay1);
+}
+
+function quizDay2(){
+    openModal()
+    document.getElementById('quizText').textContent = '우리가 처음 만난 날은? (YYYYMMDD)'
+    document.getElementById('submitAnswer').addEventListener('click', answerDay2);
+}
+
+function quizDay3(){
+    openModal()
+    document.getElementById('quizText').textContent = '내가 예지한테 가장 많이 보낸 카톡은?'
+    document.getElementById('submitAnswer').addEventListener('click', answerDay3);
+}
+
+function quizDay4(){
+    openModal()
+    document.getElementById('quizText').textContent = '우리가 처음 만난 곳은 어떤 역일까?'
+    document.getElementById('submitAnswer').addEventListener('click', answerDay4);
+}
+
+function quizDay5(){
+    openModal()
+    document.getElementById('quizText').textContent = '우리가 처음 간 식당은?'
+    document.getElementById('submitAnswer').addEventListener('click', answerDay5);
+}
+
+
+$(document).ready(function () {
+    // const myDate = new Date(); 
+    const myDate = new Date("2023-12-25"); 
+    const xmas = Date.parse("Dec 25, "+myDate.getFullYear()) 
+    const today = Date.parse(myDate) 
+
+    const daysToChristmas = Math.round((xmas-today)/(1000*60*60*24)) 
+
+    if (myDate >= new Date("2023-12-01")){
+        document.getElementById('day1').addEventListener('click', quizDay1);    
+    }
+
+    if (myDate >= new Date("2023-12-02")){
+        document.getElementById('day2').addEventListener('click', quizDay2);    
+    }
+
+    if (myDate >= new Date("2023-12-03")){
+        document.getElementById('day3').addEventListener('click', quizDay3);    
+    }
+
+    if (myDate >= new Date("2023-12-04")){
+        document.getElementById('day4').addEventListener('click', quizDay4);    
+    }
+
+    if (myDate >= new Date("2023-12-05")){
+        document.getElementById('day5').addEventListener('click', quizDay5);    
+    }
+
+    if (myDate >= new Date("2023-12-06")){
+        document.getElementById('day6').addEventListener('click', quizDay6);    
+    }
+
+    if (myDate >= new Date("2023-12-07")){
+        document.getElementById('day7').addEventListener('click', quizDay7);    
+    }
+
+    if (myDate >= new Date("2023-12-08")){
+        document.getElementById('day8').addEventListener('click', quizDay8);    
+    }
+
+    if (myDate >= new Date("2023-12-09")){
+        document.getElementById('day9').addEventListener('click', quizDay9);    
+    }
+
+    if (myDate >= new Date("2023-12-10")){
+        document.getElementById('day10').addEventListener('click', quizDay10);    
+    }
+    
+
+    
+
+    // if (daysToChristmas == 0) 
+    // $('#days').text("It's Christmas!! Merry Christmas!");
+
+    // if (daysToChristmas < 0) 
+    // $('#days').text("Christmas was "+-1*(daysToChristmas)+" days ago.");
+
+    // if (daysToChristmas > 0) {
+    //     $('#days').text(daysToChristmas+" days to Christmas!");
+    //     console.log('hi')
+    // }
+
+});
